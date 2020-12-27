b35a7606a65249f2361a23557c253faefaf8f14b
k12ish <k4krish@gmail.com>
2020-12-27 15:27:21 +0000

    Added `generate_stats_data`
    The code is still illegible.

--- __init__.py
+++ __init__.py
@@ -1,32 +1,126 @@
 # import the main window object (mw) from aqt
-from aqt import mw
+from aqt import mw, gui_hooks
 # import the "show info" tool from utils.py
 from aqt.utils import showInfo
 # import all of the Qt GUI library
 from aqt.qt import QAction
 
+from anki.utils import ids2str
+
+from aqt.stats import NewDeckStats
+from aqt.webview import WebContent
 import sys
-# We're going to add a menu item below. First we want to create a function to
-# be called when the menu item is activated.
+import os
+
+ADDON_PACKAGE = mw.addonManager.addonFromModule(__name__)
+"/_addons/{ADDON_PACKAGE}/web/my-addon.js"
 
 
-def testFunction():
-    # get the number of cards in the current collection, which is stored in
-    # the main window
-    # show a message box
-    # showInfo(repr(cardCount))
-    item = mw.col.decks.deck_tree().children
-    sys.stderr.write(str(dir(item)))
+def generate_stats_data():
+    names_and_ids = mw.col.decks.all_names_and_ids()
+    ids, labels, parents, card_counts = [], [], [], []
+    for item in names_and_ids:
+        ids.append(str(item.name))
+        split_name = [''] + str(item.name).rsplit('::', 1)
+        labels.append(split_name[-1])
+        parents.append(split_name[-2])
+        card_counts.append(
+            mw.col.db.scalar(
+                "select count() from cards where did in {0} or "
+                "odid in {0}".format(ids2str([item.id]))
+            )
+        )
+    sys.stderr.write(
+        """<script type="text/javascript">
+var data = [{{
+  type: "sunburst",
+  ids: {},
+  labels: {},
+  parents: {},
+  values: {},
+  branchvalues: 'remainder',
+  sort: false,
+}}];
+var layout = {{
+  margin: {{l: 0, r: 0, b: 0, t:0}},
+}};
+Plotly.newPlot('plotly-sunburst', data, layout)
+</script>
+""".format(
+            str(ids),
+            str(labels),
+            str(parents),
+            str(card_counts)
+        ))
+    return ids, labels, parents, card_counts
 
+
+def mytest(web):
+    page = os.path.basename(web.page().url().path())
+    if page != "graphs.html":
+        return
+    web.eval(
+        """
+    div = document.createElement("div");
+    div.setAttribute("id", "plotly-sunburst");
+    document.body.appendChild(div);
+"""
+    )
+
+
+gui_hooks.webview_did_inject_style_into_page.append(mytest)
 
 """
-<class 'backend_pb2.DeckNameID'>
+106ms
+def generate_stats_data():
+    start = time.time()
+    names_and_ids = mw.col.decks.all_names_and_ids()
+    ids, labels, parents, card_counts = [], [], [], []
+    for item in names_and_ids:
+        ids.append(str(item.name))
+        split_name = [''] + str(item.name).rsplit('::', 1)
+        labels.append(split_name[-1])
+        parents.append(split_name[-2])
+        card_counts.append(
+            mw.col.db.scalar(
+                "select count() from cards where did in {0} or "
+                "odid in {0}".format(ids2str([item.id]))
+            )
+        )
+    sys.stderr.write("\n\n`generate_stats_data` took: "
+                     + str(round((time.time() - start) * 1000))
+                     + "ms")
+    sys.stderr.write(str(labels))
+    sys.stderr.write(str(card_counts))
+    sys.stderr.write(str(parents))
+    sys.stderr.write(str(labels))
+"""
+"""
+>>> x = "aaaaa::bbbb::cccc"
+>>> y = "aaaaa::bbbbbb"
+>>> z = "aaaaaaaaa"
+>>> x.rsplit("::", 1)
+['aaaaa::bbbb', 'cccc']
+>>> y.rsplit("::", 1)
+['aaaaa', 'bbbbbb']
+>>> z.rsplit("::", 1)
+['aaaaaaaaa']
 """
 
 
-# create a new menu item, "test"
+def on_stats_dialog_will_show(dialog: NewDeckStats):
+    showInfo(str(dialog))
+
+
+def on_webview_will_set_content(web_content: WebContent, context):
+    web_content.body += "my_html"
+    web_content.head += ""
+
+
+gui_hooks.stats_dialog_will_show.append(on_stats_dialog_will_show)
+gui_hooks.webview_will_set_content.append(on_webview_will_set_content)
+
+
 action = QAction("test", mw)
-# set it to call testFunction when it's clicked
-action.triggered.connect(testFunction)
-# and add it to the tools menu
+action.triggered.connect(generate_stats_data)
 mw.form.menuTools.addAction(action)
